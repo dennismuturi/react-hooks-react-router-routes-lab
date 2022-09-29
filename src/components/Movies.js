b432b87cca3d2bfc0eeaf92ff40b33f3668454ed
dennismuturi <dennismuturi1@gmail.com>
2022-09-29 11:33:29 +0300

    initial commit

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,28 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Movie Page</h1>
+    {
+      movies.map((movie)=>{
+        <div>
+          <ul>
+            <li>{movie.title}</li>
+            <li>{movie.time}</li>
+            <li>
+              <ul>
+                {
+                  movie.genres.map((genre,index)=>{
+                    <li key={index}>{genre}</li>
+                  })
+                }
+              </ul>
+              </li>
+            </ul>
+        </div>
+      })
+    }
+  </div>;
 }
 
 export default Movies;
